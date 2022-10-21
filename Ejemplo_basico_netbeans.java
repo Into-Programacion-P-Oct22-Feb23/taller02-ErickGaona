223a5bde23879e22077505b645fa27554fe63266
Erick Gaona <erickgaona@gmail.com>
2022-10-21 10:44:00 -0500

    primer commit

---
+++ Ejemplo_basico_netbeans.java
@@ -0,0 +1,19 @@
+
+package ejemplo_basico_netbeans;
+
+/**
+ *
+ * @author Erick
+ */
+public class Ejemplo_basico_netbeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Actividad en clase");
+        
+    }
+    
+}
