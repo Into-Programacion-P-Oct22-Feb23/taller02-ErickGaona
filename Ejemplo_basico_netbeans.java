01b81b9468c81a7959695d11d97a49270ef72cfd
Erick Gaona <erickgaona@gmail.com>
2022-10-21 10:47:27 -0500

    segundo commit

--- Ejemplo_basico_netbeans.java
+++ Ejemplo_basico_netbeans.java
@@ -12,6 +12,11 @@ public class Ejemplo_basico_netbeans {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+    //Ejemplo de Java
+    //Manejado desde GitHub
+    //Versionado a través de Git
+    //Creado por <ubicar su usuario de github>
         System.out.println("Actividad en clase");
         
     }
